0f101cc98fd536520c4e9cca6a7daa76c6b6ff1c
Martin Popel <popel@ufal.mff.cuni.cz>
2017-02-16 03:54:27 +0100

    fix UDv2 problems with ud.Convert1to2 skip=coord,nmod

---
+++ not-to-release/convert.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+udapy -h >/dev/null || { echo "udapy is not installed, see https://github.com/udapi/udapi-python"; exit 1; }
+
+SCENARIO="ud.Convert1to2 skip=coord,nmod"
+for a in train dev test; do
+    cat ru-ud-$a.conllu | udapy -s $SCENARIO > ../ru-ud-$a.conllu
+done
+
+cat ../ru-ud-{train,dev,test}.conllu | udapy -HAMC ud.MarkBugs skip='no-(VerbForm|PronType)' > bugs.html
