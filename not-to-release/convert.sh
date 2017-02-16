fb5b5b7f628277d6eb996364b537ff3baaf09014
Martin Popel <popel@ufal.mff.cuni.cz>
2017-02-16 03:56:48 +0100

    fix three remaining sentences with remnants

--- not-to-release/convert.sh
+++ not-to-release/convert.sh
@@ -2,7 +2,7 @@
 
 udapy -h >/dev/null || { echo "udapy is not installed, see https://github.com/udapi/udapi-python"; exit 1; }
 
-SCENARIO="ud.Convert1to2 skip=coord,nmod"
+SCENARIO="ud.Convert1to2 skip=coord,nmod ud.ru.FixRemnant"
 for a in train dev test; do
     cat ru-ud-$a.conllu | udapy -s $SCENARIO > ../ru-ud-$a.conllu
 done
